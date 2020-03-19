c8d357749d82b3bebf33d9ef018268855b376fd0
John Ericksen <johncarl81@gmail.com>
2020-03-19 23:41:21 +0000

    Added command.py to startup script

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]
+if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" ]]
 then
-    echo "Usage: ./startup.sh <dragonfly apm namespace>"
+    echo "Usage: ./startup.sh <dragonfly apm namespace> <unique altitude> <index in swarm> <swarm size>"
     exit 1
 fi
 
@@ -13,5 +13,6 @@ exec 3>&1 1>>logs/script.log 2>&1
 #roscore&
 sleep 10
 roslaunch apm.launch & 
-python co2publisher.py $1 &
-python logger.py $1 >> logs/output.log &
+./co2publisher.py $1 &
+./logger.py $1 >> logs/output.log &
+./command.py $1 $2 $3 $4 &
