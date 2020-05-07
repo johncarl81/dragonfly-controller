a3ea2979d7138bb86198ef5ca615d1bfc591b9c1
John Ericksen <johncarl81@gmail.com>
2020-05-07 16:16:17 +0000

    Added graceful failure and retry to co2publisher

--- src/dragonfly/scripts/co2publisher.py
+++ src/dragonfly/scripts/co2publisher.py
@@ -5,21 +5,24 @@ import argparse
 from std_msgs.msg import String
 
 def publishco2(id):
-    print "publishing co2 readings on {}/co2".format(id)
+    rospy.loginfo("publishing co2 readings on {}/co2".format(id))
     pub = rospy.Publisher("{}/co2".format(id), String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(1) # 10hz
-    with serial.Serial("/dev/ttysba5", baudrate=19200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE) as port:
-        # Publish on demand
-        port.write('!')
-        while True:
-            port.write('M')
-            hello_str = port.readline()
-            rospy.loginfo(hello_str)
-            pub.publish(hello_str)
-            rate.sleep()
-
-
+    while True:
+        try:
+            with serial.Serial("/dev/ttysba5", baudrate=19200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE) as port:
+                rospy.loginfo('Connected to /dev/ttysba5') 
+                # Publish on demand
+                port.write('!')
+                while True:
+                    port.write('M')
+                    hello_str = port.readline()
+                    pub.publish(hello_str)
+                    rate.sleep()
+        except serial.SerialException as ex:
+            rospy.loginfo('Connection to /dev/ttysba5 failed, retrying...')
+            rospy.sleep(1)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Starts ROS publisher for CO2 sensor.')
