4e38fc55a6fae89ecbf7e74d5cbf7663771a64b3
John Ericksen <johncarl81@gmail.com>
2020-05-15 23:54:56 +0000

    Updated run.sh to readd logging and command script

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ source ./devel/setup.bash
 
 exec 3>&1 1>>logs/script.log 2>&1
 
-sudo route add -net 224.0.0.0 netmask 240.0.0.0 wlxdc4ef406d9e8
+sudo route add -net 224.0.0.0 netmask 240.0.0.0 wlan0
 rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.0 &
 P1=$!
 sleep 3
@@ -20,7 +20,8 @@ roslaunch config/apm.launch &
 P3=$!
 rosrun dragonfly co2publisher.py $1 &
 P4=$!
-rosrun dragonfly logger.py $1 > output.log &
+rosrun dragonfly logger.py $1 >> logs/run.log &
 P5=$!
-rosrun dragonfly command.py $1 1 0 > output.log &
-wait $P1 $P2 $P3 $P4 $P5
+rosrun dragonfly command.py $1 1 0 >> logs/command.log &
+P6=$!
+wait $P1 $P2 $P3 $P4 $P5 $P6
