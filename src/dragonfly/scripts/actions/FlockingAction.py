fa22ddc3f5a02a3fc710f3aad287ad9c61d64382
John Ericksen <johncarl81@gmail.com>
2020-11-28 11:57:13 -0700

    Added Flocking velocity matching, formation position, and repulsion forces

--- src/dragonfly/scripts/actions/FlockingAction.py
+++ src/dragonfly/scripts/actions/FlockingAction.py
@@ -5,6 +5,7 @@ from sensor_msgs.msg import NavSatFix
 from std_msgs.msg import String
 from ActionState import ActionState
 from rx.subjects import Subject
+from rx.core import Observable
 
 class FlockingAction:
 
@@ -20,51 +21,95 @@ class FlockingAction:
         self.leaderposition_subject = Subject()
         self.selfposition_subject = Subject()
         self.leadervelocity_subject = Subject()
+        self.flock_repulsion = Subject()
 
-        self.leaderposition_subject.zip(self.selfposition_subject, self.leadervelocity_subject, lambda a, b, c: [a,b,c]) \
-                .subscribe(lambda value: self.printStuff(value))
+        formation_position_attraction = Observable.zip(self.leaderposition_subject, self.selfposition_subject,
+                                                       lambda leaderposition, selfposition: self.formation_position(leaderposition, selfposition))
 
-    def printStuff(self, input):
+        leaderVelocity = self.leadervelocity_subject \
+            .map(lambda twist: self.format_velocities(twist))
 
+        Observable.zip_array(leaderVelocity, formation_position_attraction, self.flock_repulsion) \
+            .subscribe(lambda vectors: self.navigate(vectors))
 
-        print "self: {}, {} target: {}, {}".format(input[0].pose.position.x, input[0].pose.position.y, input[1].pose.position.x, input[1].pose.position.y)
+        self.flockSubscription = Observable.empty().subscribe()
 
-        # TODO: need to property orient between self and the lead - maybe using a TF?
-        offsetx = input[0].pose.position.x - input[1].pose.position.x + self.xoffset
-        offsety = input[0].pose.position.y - input[1].pose.position.y + self.yoffset
-        # print input
+    def navigate(self, input):
+
         twist = TwistStamped()
-        twist.twist.linear.x = input[2].twist.linear.x + (0.3 * offsetx)
-        twist.twist.linear.y = input[2].twist.linear.y + (0.3 * offsety)
 
-        print "Publishing velocity ({},{}) twist: {}".format(twist.twist.linear.x, twist.twist.linear.y, twist.twist.angular.z)
+        print "Magnitudes: {}".format([self.magnitude(v) for v in input])
+
+        for vector in input:
+            twist.twist.linear.x += vector[0]
+            twist.twist.linear.y += vector[1]
+
         self.local_setvelocity_publisher.publish(twist)
 
+    def differenceInMeters(self, one, two):
+        earthCircumference = 40008000
+        return [
+            ((one.longitude - two.longitude) * (earthCircumference / 360) * math.cos(one.latitude * 0.01745)),
+            ((one.latitude - two.latitude) * (earthCircumference / 360))
+        ]
 
-    def leaderposition_callback(self, pose):
-        # print "Received pose"
-        self.leaderposition_subject.on_next(pose)
+    def formation_position(self, leaderPosition, selfPosition):
+        difference = self.differenceInMeters(leaderPosition, selfPosition)
 
-    def selfposition_callback(self, pose):
-        # print "Received pose"
-        self.selfposition_subject.on_next(pose)
+        return [
+            (0.3 * (difference[0] + self.xoffset)),
+            (0.3 * (difference[1] + self.yoffset))
+        ]
 
-    def leadervelocity_callback(self, twist):
-        # print "Received twist"
-        self.leadervelocity_subject.on_next(twist)
+    def format_velocities(self, twist):
+        return [
+            twist.twist.linear.x,
+            twist.twist.linear.y
+        ]
+
+    def magnitude(self, vector):
+        return math.sqrt((vector[0] * vector[0]) + (vector[1] * vector[1]))
+
+    def repulsion_vector(self, positions):
+        equilibrium_distance = 3
+
+        vector = [0,0]
+        if len(positions) > 1:
+            self_position = positions[0]
+            for position in positions[1:]:
+                difference = self.differenceInMeters(self_position, position)
+                difference_magnitude = self.magnitude(difference)
+                if difference_magnitude < equilibrium_distance:
+                    vector[0] += (equilibrium_distance - difference_magnitude) * difference[0] / difference_magnitude
+                    vector[1] += (equilibrium_distance - difference_magnitude) * difference[1] / difference_magnitude
+
+        return vector
+
+    def flock_announce_callback(self, nameString):
+        self.flock_announce(nameString.data)
 
     def flock_announce(self, name):
-        if name.data not in self.flock_coordinates :
+        if name != self.id and name not in self.flock_coordinates :
+            print "Registering flock member: {}".format(name)
             flock_coordinate_subject = Subject()
-            self.flock_coordinates[name.data] = flock_coordinate_subject
+            self.flock_coordinates[name] = flock_coordinate_subject
 
             def flock_coordiante_subject(position):
-                print "name: {} position: {} {}".format(name.data, position.latitude, position.longitude)
+                # print "name: {} position: {} {}".format(name, position.latitude, position.longitude)
                 flock_coordinate_subject.on_next(position)
 
-            rospy.Subscriber("{}/mavros/global_position/global".format(name.data), NavSatFix, flock_coordiante_subject)
+            rospy.Subscriber("{}/mavros/global_position/global".format(name), NavSatFix, flock_coordiante_subject)
+
+            self.flockSubscription.dispose()
+
+            flock_coordinate_subject_list = [self.selfposition_subject]
+            flock_coordinate_subject_list.extend(self.flock_coordinates.values())
 
 
+
+            self.flockSubscription = Observable.zip_array(*flock_coordinate_subject_list) \
+                    .map(lambda positions: self.repulsion_vector(positions)) \
+                    .subscribe(on_next= lambda v: self.flock_repulsion.on_next(v))
 
     def step(self):
         if not self.started:
@@ -72,10 +117,10 @@ class FlockingAction:
 
             print "Subscribing..."
 
-            rospy.Subscriber("{}/mavros/local_position/pose".format(self.leader), PoseStamped, self.leaderposition_callback)
-            rospy.Subscriber("{}/mavros/local_position/pose".format(self.id), PoseStamped, self.selfposition_callback)
-            rospy.Subscriber("{}/mavros/local_position/velocity_local".format(self.leader), TwistStamped, self.leadervelocity_callback)
-            # rospy.Subscriber("/dragonfly/announce", String, self.flock_announce)
+            rospy.Subscriber("{}/mavros/global_position/global".format(self.leader), NavSatFix, lambda position: self.leaderposition_subject.on_next(position))
+            rospy.Subscriber("{}/mavros/global_position/global".format(self.id), NavSatFix, lambda position: self.selfposition_subject.on_next(position))
+            rospy.Subscriber("{}/mavros/local_position/velocity_local".format(self.leader), TwistStamped, lambda twist: self.leadervelocity_subject.on_next(twist))
+            rospy.Subscriber("/dragonfly/announce", String, self.flock_announce_callback)
 
 
         return ActionState.WORKING
