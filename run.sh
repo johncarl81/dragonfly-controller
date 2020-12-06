79f184efdaadcb6db019101778b1fe5ed6e14954
John Ericksen <johncarl81@gmail.com>
2020-12-06 01:46:31 +0000

    Externalized required name and system id from apm.launch script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]
+if [[ -z "$1" || -z "$2" ]]
 then
-    echo "Usage: ./run.sh <dragonfly apm namespace>"
+    echo "Usage: ./run.sh <dragonfly apm namespace> <SYSID_THISMAV for this instance>"
     exit 1
 fi
 
@@ -15,7 +15,7 @@ rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.0 &
 P1=$!
 rosrun master_sync_fkie master_sync &
 P2=$!
-/opt/ros/kinetic/bin/roslaunch /home/ubuntu/dev/dragonfly/config/apm.launch &
+/opt/ros/kinetic/bin/roslaunch /home/ubuntu/dev/dragonfly/config/apm.launch name:=$1 tgt_system:=$2 &
 P3=$!
 rosrun dragonfly co2publisher.py $1 &
 P4=$!
