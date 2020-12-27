44337a79e95daa81f3295cd87ebfce9f20298573
John Ericksen <johncarl81@gmail.com>
2020-12-27 13:10:58 -0700

    Added mission service and related messages

--- rundocker.sh
+++ rundocker.sh
@@ -2,4 +2,8 @@
 docker run -it \
     --network ros-net \
     dragonfly-controller:latest \
-    /bin/sh -c 'rosrun dragonfly announce.py JUAV1 & rosrun dragonfly command.py JUAV1'
+    /bin/sh -c 'for i in {1..3}
+    do
+      rosrun dragonfly announce.py JUAV$i &
+      rosrun dragonfly command.py JUAV$i &
+    done; rosrun dragonfly announce.py JUAV4 & rosrun dragonfly command.py JUAV4'
