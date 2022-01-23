561499330c703f88bf5ecf33d5c27094b0b271e9
John Ericksen <johncarl81@gmail.com>
2022-01-23 11:51:17 -0700

    Add ros-net if it doesnt exist

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+docker network inspect ros-net >/dev/null 2>&1 || \
+    docker network create ros-net --subnet=172.18.0.0/16
+
 docker run -it \
     --network ros-net \
     dragonfly-controller:latest \
