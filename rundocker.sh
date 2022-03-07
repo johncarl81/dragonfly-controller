3c4d963802ed54842df7dd484dbda44b26590b49
Carter Frost <git@carterfrost.com>
2022-03-06 23:06:24 -0700

    rundocker.sh: revert to support 4 drones & update for ros2

--- rundocker.sh
+++ rundocker.sh
@@ -6,7 +6,13 @@ docker network inspect ros-net >/dev/null 2>&1 || \
 docker run -it \
     --network ros-net \
     dragonfly-controller:latest \
-    /bin/sh -c '
-    ros2 run dragonfly announce dragonfly1 &
-    ros2 run dragonfly command dragonfly1 &
-    ros2 run dragonfly virtualco2publisher dragonfly1'
+    /bin/sh -c 'for i in {1..3}
+    do
+      ros2 run dragonfly announce dragonfly$i &
+      ros2 run dragonfly command dragonfly$i &
+      ros2 run dragonfly virtualco2publisher dragonfly$i &
+    done;
+    ros2 run dragonfly announce dragonfly4 &
+    ros2 run dragonfly command dragonfly4 &
+    ros2 run dragonfly virtualco2publisher dragonfly4'
+    
