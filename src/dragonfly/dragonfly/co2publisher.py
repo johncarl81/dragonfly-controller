4dfa94d204d25cf0971b6223967f176643b93c6f
Carter Frost <carter90@gmail.com>
2022-07-03 20:41:35 -0600

    src/dragonfly/dragonfly/co2publisher.py: fix spacing, id->args, python3

--- src/dragonfly/dragonfly/co2publisher.py
+++ src/dragonfly/dragonfly/co2publisher.py
@@ -9,43 +9,41 @@ from std_msgs.msg import String
 from rclpy.qos import HistoryPolicy
 
 
-def publishco2(id):
-    rclpy.init()  # args=id
-    node = rclpy.create_node('co2_publisher')
-    node.get_logger().info("publishing co2 readings on {}/co2".format(id))
-    pub = node.create_publisher(String, "{}/co2".format(id), qos_profile=QoSProfile(history=HistoryPolicy.KEEP_LAST, depth=10))
-    rate = node.create_rate(10)
-    while rclpy.ok():
-        try:
-            with serial.Serial("/dev/ttysba5", baudrate=19200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
-                               stopbits=serial.STOPBITS_ONE) as port:
-                node.get_logger().info('Connected to /dev/ttysba5')
-                # Publish on demand
-                port.write(str.encode('!')) 
-                # Configure to 2 decimal places
-                port.write(str.encode('C2\r'))
-                while rclpy.ok():
-                    port.write(str.encode('M'))
-                    hello_str = port.readline()
-                    
-                    #@TODO split and put into 
-                    #/home/carter/dfly/dragonfly-controller/src/dragonfly_messages/msg/CO2.msg
-                    
-                    
-                    pub.publish(hello_str)
-                    rate.sleep()
-        except serial.SerialException as ex:
-            node.get_logger().warn("SerialException" + str(ex))
-            rate.sleep()
+def publishco2(args):
+  rclpy.init()  # args=id
+  node = rclpy.create_node('co2_publisher')
+  node.get_logger().info("publishing co2 readings on {}/co2".format(args))
+  pub = node.create_publisher(String, "{}/co2".format(args),
+                              qos_profile=QoSProfile(history=HistoryPolicy.KEEP_LAST, depth=10))
+  rate = node.create_rate(10)
+  while rclpy.ok():
+    try:
+      with serial.Serial("/dev/ttysba5", baudrate=19200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
+                         stopbits=serial.STOPBITS_ONE) as port:
+        node.get_logger().info('Connected to /dev/ttysba5')
+        # Publish on demand
+        port.write(str.encode('!'))
+        # Configure to 2 decimal places
+        port.write(str.encode('C2\r'))
+        while rclpy.ok():
+          port.write(str.encode('M'))
+          hello_str = port.readline()
+          # @TODO split and put into
+          # /home/carter/dfly/dragonfly-controller/src/dragonfly_messages/msg/CO2.msg
+          pub.publish(hello_str)
+          rate.sleep()
+    except serial.SerialException as ex:
+      node.get_logger().warn("SerialException" + str(ex))
+      rate.sleep()
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Starts ROS publisher for CO2 sensor.')
-    parser.add_argument('id', type=str, help='Name of the drone.')
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser(description='Starts ROS publisher for CO2 sensor.')
+  parser.add_argument('id', type=str, help='Name of the drone.')
+  args = parser.parse_args()
 
-    publishco2(args.id)
+  publishco2(args.id)
 
 
 if __name__ == '__main__':
-    main()
+  main()
