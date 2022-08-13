eace75294aff2522f6178f4236841bf16c8e740d
Carter Frost <carter90@gmail.com>
2022-08-12 21:04:03 -0600

    src/dragonfly/dragonfly/pump.py: service node creation

---
+++ src/dragonfly/dragonfly/pump.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import argparse
+import time
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Bool
+import RPi.GPIO as GPIO
+
+
+class BagInflateService(Node):
+  def __init__(self, arg_id):
+    super().__init__('bag_inflate_service')
+    self.id = arg_id
+    self.bag_gpio_pins = [16, 19, 20, 21]
+    self.bag_full = [False] * len(self.bag_gpio_pins)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(self.bag_gpio_pins, GPIO.OUT)
+    self.inflate = self.create_service(Bool, "/{}/pump".format(self.id), self.bag_inflate_callback)
+    self.swap = self.create_service(Bool, "/{}/bagswap".format(self.id), self.bag_swap_callback)
+
+  def bag_swap_callback(self, request):
+    self.get_logger().info("Bags swapped")
+    self.bag_full = [False] * len(self.bag_gpio_pins)
+    return Bool(True)
+
+  def bag_inflate_callback(self, request):
+    # @TODO add timestamp and id ect
+    self.get_logger().info("Bag inflate request received")
+    if not self.bag_full[request.pump_num]:
+      self.bag_full[request.pump_num] = True
+      GPIO.output(self.bag_gpio_pins[request.pump_num], 1)
+      time.sleep(60)
+      GPIO.output(self.bag_gpio_pins[request.pump_num], 0)
+      return Bool(True)
+    self.get_logger().warn("Bag already inflated")
+    return Bool(False)
+
+
+def main(args=None):
+  parser = argparse.ArgumentParser(description='Log the given drone\'s GPS And CO2.')
+  parser.add_argument('id', type=str, help='Name of the drone.')
+  args = parser.parse_args()
+  rclpy.init(args=args)
+  bag_inflate_service = BagInflateService(args.id)
+  rclpy.spin(bag_inflate_service)
+  rclpy.shutdown()
+  GPIO.cleanup()
+
+
+if __name__ == '__main__':
+  main()
