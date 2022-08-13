a714ee5d768a06e09a94cff8e11755ff2f254afe
Carter Frost <carter90@gmail.com>
2022-08-13 16:50:57 -0600

    src/dragonfly/dragonfly/pump.py: rx, spacing, sim arg/check, unwrap bool

--- src/dragonfly/dragonfly/pump.py
+++ src/dragonfly/dragonfly/pump.py
@@ -4,52 +4,66 @@ import argparse
 import time
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Bool
 from dragonfly_messages.srv import Pump
-import RPi.GPIO as GPIO
+import rx
+import rx.operators as ops
+from rx.scheduler import NewThreadScheduler
+
 
 class BagInflateService(Node):
-  def __init__(self, arg_id):
-    super().__init__('bag_inflate_service')
-    self.id = arg_id
-    self.bag_gpio_pins = [16, 19, 20, 21]
-    self.bag_full = [False] * len(self.bag_gpio_pins)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(self.bag_gpio_pins, GPIO.OUT)
-    self.inflate = self.create_service(Pump, "/{}/pump".format(self.id), self.bag_inflate_callback)
-    #self.swap = self.create_service(BagSwap, "/{}/bagswap".format(self.id), self.bag_swap_callback)
+    def __init__(self, arg_id, sim=False):
+        super().__init__('bag_inflate_service')
+        self.id = arg_id
+        self.sim = sim
+        if not self.sim:
+            import RPi.GPIO as GPIO
+            self.bag_gpio_pins = [16, 19, 20, 21]
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setwarnings(False)
+            GPIO.setup(self.bag_gpio_pins, GPIO.OUT)
+        self.bag_full = [False] * len(self.bag_gpio_pins)
+        self.inflate = self.create_service(Pump, "/{}/pump".format(self.id), self.bag_inflate_callback)
+        # self.swap = self.create_service(BagSwap, "/{}/bagswap".format(self.id), self.bag_swap_callback)
 
-  def bag_swap_callback(self, request, response):
-    self.get_logger().info("Bags swapped")
-    self.bag_full = [False] * len(self.bag_gpio_pins)
-    return Bool(True)
+    def bag_swap_callback(self, request, response):
+        self.get_logger().info("Bags swapped")
+        self.bag_full = [False] * len(self.bag_gpio_pins)
+        return True
 
-  def bag_inflate_callback(self, request, response):
-    # @TODO add timestamp and id ect
-    self.get_logger().info("Bag inflate request received")
-    if not self.bag_full[request.pump_num]:
-      self.bag_full[request.pump_num] = True
-      GPIO.output(self.bag_gpio_pins[request.pump_num], 1)
-      time.sleep(60)
-      GPIO.output(self.bag_gpio_pins[request.pump_num], 0)
-      response.done = Bool(True)
-      return response
-    self.get_logger().warn("Bag already inflated")
-    response.done = Bool(False)
-    return response
+    def bag_inflate_callback(self, request, response):
+        # @TODO add timestamp and id ect
+        self.get_logger().info("Bag inflate request received")
+        if not self.bag_full[request.pump_num]:
+            self.bag_full[request.pump_num] = True
+            if not self.sim:
+                rx.just(self.bag_gpio_pins[request.pump_num]).pipe(
+                    ops.observe_on(NewThreadScheduler())) \
+                    .subscribe(on_next=lambda pin: self.pump(pin))
+
+            response.done = True
+            return response
+        self.get_logger().warn("Bag already inflated")
+        response.done = False
+        return response
+
+    def pump(self, pin):
+        if not self.sim:
+            GPIO.output(pin, 1)
+            time.sleep(60)
+            GPIO.output(pin, 0)
 
 
 def main(args=None):
-  parser = argparse.ArgumentParser(description='Sample Bag Collection Service')
-  parser.add_argument('id', type=str, help='Name of the drone.')
-  args = parser.parse_args()
-  rclpy.init(args=sys.argv)
-  bag_inflate_service = BagInflateService(args.id)
-  rclpy.spin(bag_inflate_service)
-  rclpy.shutdown()
-  GPIO.cleanup()
+    parser = argparse.ArgumentParser(description='Sample Bag Collection Service')
+    parser.add_argument('id', type=str, help='Name of the drone.')
+    parser.add_argument('sim', type=bool, help='Is the sim running')
+    args = parser.parse_args()
+    rclpy.init(args=sys.argv)
+    bag_inflate_service = BagInflateService(args.id, args.sim)
+    rclpy.spin(bag_inflate_service)
+    rclpy.shutdown()
+    GPIO.cleanup()
 
 
 if __name__ == '__main__':
-  main(args=sys.argv)
+    main(args=sys.argv)
