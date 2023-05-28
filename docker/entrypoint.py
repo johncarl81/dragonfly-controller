97a324464c070bab2e87de9ad0f5c22d57e0ea88
Carter Frost <git@carterfrost.com>
2023-05-27 22:14:53 -0600

    startup.sh|docker/entrypoint.py: append .py to executables

--- docker/entrypoint.py
+++ docker/entrypoint.py
@@ -7,10 +7,10 @@ def run_controllers(args):
     # Start dragonfly controllers
     for i in range(0, args.drones):
         name = f"dragonfly{i + 1}"
-        processes.append(subprocess.Popen(f"/entrypoint.sh ros2 run dragonfly announce {name}", shell=True))
-        processes.append(subprocess.Popen(f"/entrypoint.sh ros2 run dragonfly command {name}", shell=True))
-        processes.append(subprocess.Popen(f"/entrypoint.sh ros2 run dragonfly virtualco2publisher {name}", shell=True))
-        processes.append(subprocess.Popen(f"/entrypoint.sh ros2 run dragonfly pump {name} --sim", shell=True))
+        processes.append(subprocess.Popen(f"/entrypoint.sh ros2 run dragonfly announce.py {name}", shell=True))
+        processes.append(subprocess.Popen(f"/entrypoint.sh ros2 run dragonfly command.py {name}", shell=True))
+        processes.append(subprocess.Popen(f"/entrypoint.sh ros2 run dragonfly virtualco2publisher.py {name}", shell=True))
+        processes.append(subprocess.Popen(f"/entrypoint.sh ros2 run dragonfly pump.py {name} --sim", shell=True))
 
     for p in processes:
         p.wait()
